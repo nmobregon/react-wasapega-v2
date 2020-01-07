df9fe0a2a6327a3cef93facc2d58e63ead78a753
Nicolas Mariano Obregon <nmobregon@thinkup.com.ar>
2020-01-07 09:17:09 -0300

    Updates

---
+++ precache-manifest.a4852f2064c886a5cc2db6788bdf17d0.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f22b63cb3fddf06795d87fe9b223616",
+    "url": "/react-wasapega-v2/index.html"
+  },
+  {
+    "revision": "6d216d6c09bca3be213b",
+    "url": "/react-wasapega-v2/static/css/2.66dfd84a.chunk.css"
+  },
+  {
+    "revision": "d860bbf8f8a6cfa79015",
+    "url": "/react-wasapega-v2/static/css/main.3fd28ca7.chunk.css"
+  },
+  {
+    "revision": "6d216d6c09bca3be213b",
+    "url": "/react-wasapega-v2/static/js/2.70c6d14e.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-wasapega-v2/static/js/2.70c6d14e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d860bbf8f8a6cfa79015",
+    "url": "/react-wasapega-v2/static/js/main.7aa861dd.chunk.js"
+  },
+  {
+    "revision": "c21e6431104f38dc73df",
+    "url": "/react-wasapega-v2/static/js/runtime-main.45c14aa9.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/react-wasapega-v2/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/react-wasapega-v2/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "81c76b18adcaa60019e2a3dd483f8900",
+    "url": "/react-wasapega-v2/static/media/logo.81c76b18.png"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/react-wasapega-v2/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "2d2afb2719a1ee903e576e7c457daf81",
+    "url": "/react-wasapega-v2/static/media/primeicons.2d2afb27.eot"
+  },
+  {
+    "revision": "66ee0deb739ca71f0ecdc39d7c1b22cb",
+    "url": "/react-wasapega-v2/static/media/primeicons.66ee0deb.woff"
+  },
+  {
+    "revision": "df0140f8e79ecfeffaf85220aaecd7c4",
+    "url": "/react-wasapega-v2/static/media/primeicons.df0140f8.ttf"
+  },
+  {
+    "revision": "e5e0e94474d5fd92e7e800a8865d297c",
+    "url": "/react-wasapega-v2/static/media/primeicons.e5e0e944.svg"
+  }
+]);
